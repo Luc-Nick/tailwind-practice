0cff31be7035974ac722b92ea00a9fe5a5ee68ae
Nicolas Lucero <lucero.nico216@gmail.com>
2023-11-24 12:37:21 -0300

    first commit tailwinds practice

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+import {shoes , statistics } from "../components";
+import { Button, ShoeCard } from "../components"
+import { bigShoe1 } from "../assets/images";
+import { arrowRight } from "../assets/icons";
+
+const Hero = () => {
+  const [bigShoeImg, setBigShoeImg] = useState(bigShoe1);
+
+  return (
+    <section 
+      id="home"
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container p-2"
+    >
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+        <p className="text-xl font-montserrat text-coral-red">
+          Our summer collections
+        </p>
+
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
+            The New Arrival
+          </span>
+          <br />
+          <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
+        </h1>
+        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
+          Discover stylish Nike arrivals, quality comfort, and innovations for your active life.
+        </p>
+
+        <Button label="Shop Now" iconURL={arrowRight} />
+
+        <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
+          {statistics.map((stat,index) => (
+            <div key={index}>
+              <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
+              <p className="leading-7 font-montserrat text-slate-gray">
+                {stat.label}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="">
+        <img src="" alt="" />
+        <div className="">
+            <ShoeCard />
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import CustomerReviews from './CustomerReviews';
+import Footer from './Footer';
+import PopularProducts from './PopularProducts';
+import Services from './Services';
+import SpecialOffers from './SpecialOffers'
+import SuperQuality from './SuperQuality'
+import Suscribe from './Suscribe'
+
+export{
+    Hero,
+    CustomerReviews,
+    Footer,
+    PopularProducts,
+    Services,
+    SpecialOffers,
+    SuperQuality,
+    Suscribe
+}
